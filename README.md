aae92930f1defc71d5f09534dac6c331e609512b
Tyler Durham <TylerDurham@users.noreply.github.com>
2024-09-15 20:18:17 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learn to Code by Solving Problems
+
+- https://nostarch.com/learn-code-solving-problems
+- https://dmoj.ca/user
---
+++ ch01/word_count.py
@@ -0,0 +1,5 @@
+words = input("Enter a line of test, and I'll count the number of words: ")
+
+count = words.count(" " ) + 1
+
+print("You entered " + str(count) + " words.")
